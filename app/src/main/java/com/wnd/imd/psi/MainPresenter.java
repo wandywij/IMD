cbf8387fbca297714253f80da809d6a47e9726d4
Wandy Wijayanto <wandy.wijayanto@gmail.com>
2017-09-19 15:46:50 +0700

    add robolectric for testing map marker

--- app/src/main/java/com/wnd/imd/psi/MainPresenter.java
+++ app/src/main/java/com/wnd/imd/psi/MainPresenter.java
@@ -6,14 +6,12 @@ import com.wnd.imd.networking.response.PSIResponseModel;
 
 import java.io.IOException;
 
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.BehaviorSubject;
-
 /**
  * Created by Wandy on 9/16/17.
  */
@@ -27,6 +25,10 @@ public class MainPresenter {
         this.interactor = interactor;
     }
 
+    /**
+     * Get PSI data from API and send signal whenever the data is collected succesfully or whenever
+     * it throws any errors.
+     * */
     public void getPSI() {
         behaviorSubject = BehaviorSubject.create();
         interactor.getPSI()
@@ -50,6 +52,15 @@ public class MainPresenter {
                 });
     }
 
+    /**
+     * Will be called whenever activity is resuming.
+     * This method will get PSI data and observe any signal receive to update map.
+     *
+     * @param getPSIResponseModel
+     * @param throwableConsumer
+     *
+     * @return Disposable that observe changes on data an update map afterwards
+     * */
     public Disposable onResume(Consumer<PSIResponseModel> getPSIResponseModel, Consumer<Throwable> throwableConsumer) {
         getPSI();
         return behaviorSubject.
@@ -61,6 +72,11 @@ public class MainPresenter {
         return new IOException(Constant.IS_NO_INTERNET_CONNECTION);
     }
 
+    /**
+     * Create a new general Exception to be thrown
+     *
+     * @return New general Exception
+     * */
     public Exception getException() {
         return new Exception();
     }
